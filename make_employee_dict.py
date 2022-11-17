551bfbf5132a285934f0ef9f2481dd3fe9a73e47
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2022-11-17 01:03:58 -0800

    commit 6

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -5,12 +5,12 @@
 
 class Employee:
     """Represents an employee for a company"""
-    def __init__(self, _name, _ID_number, _salary, _email_add):
+    def __init__(self, _name, _ID_number, _salary, _email_address):
         """Creates an employee profile with private data of name, id number, salary, and email"""
         self._name=_name
         self._ID_number=_ID_number
         self._salary=_salary
-        self._email_add=_email_add
+        self._email_address=_email_address
 
     def get_name(self):
         """returns name"""
@@ -21,21 +21,21 @@ class Employee:
     def get_salary(self):
         """returns salary"""
         return self._salary
-    def get_email_add(self):
+    def get_email_address(self):
         """returns email_add"""
-        return self._email_add
+        return self._email_address
 
-def make_employee_dict(_name, _ID_number, _salary, _email_add):
+def make_employee_dict(name, ID_number, salary, email_address):
     """creates dictionary for employee data, so when key id number is known, their name, email, and salary can be returned"""
     dict={} #creates empty dictionary for employee data
-    for i in range(len(_ID_number)): #sets length of employee info
-        dict[_ID_number[i]]=Employee(_name[i], _ID_number[i], _salary[i], _email_add[i]) #sets id number up as key for private data
+    for i in range(len(ID_number)): #sets length of employee info
+        dict[ID_number[i]]=Employee(name[i], ID_number[i], salary[i], email_address[i]) #sets id number up as key for private data
     return dict #returns filled dictionary
 
 #testing
 #name= ["Jean", "Kat", "Pomona"]
 #ID_number=["100", "101", "102"]
 #salary=[30, 35, 28]
-#email_add=["Jean@aol.com", "Kat@aol.com", "Pomona@aol.com"]
-#result= make_employee_dict(name, ID_number, salary, email_add)
+#email_address=["Jean@aol.com", "Kat@aol.com", "Pomona@aol.com"]
+#result= make_employee_dict(name, ID_number, salary, email_address)
 #print(result["100"].get_name())
